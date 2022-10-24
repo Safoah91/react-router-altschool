83b9b2179c78aa0ba87546e64bc02672b04e132e
Safoah91 <lampteyanastasia@gmail.com>
2022-10-24 18:11:41 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home'
+import Navbar from './components/Navbar';
+import Services from './components/Services';
+// import Users from './components/Users';
+import Contact from './components/Contact';
+import PageNotFound from './components/PageNotFound';
+import FeaturedProjects from './components/FeaturedProjects';
+import LatestProjects from './components/LatestProjects';
+import React from 'react';
+import ErrorBoundary from './components/ErrorBoundary';
+
+const LazyUser = React.lazy(() => import('./components/Users'))
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <ErrorBoundary>
+        <Routes>
+          <Route path='/' element={<Home />}>
+            <Route index element={<FeaturedProjects />} />
+            <Route path='featured-projects' element={<FeaturedProjects />} />
+            <Route path='latest-projects' element={<LatestProjects />} />
+          </Route>
+          <Route path='/services' element={<Services />} />
+          <Route path='/users' element={
+            <React.Suspense fallback='Loading...'>
+              <LazyUser />
+            </React.Suspense>
+          } />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </ErrorBoundary>
+    </div>
+  );
+}
+
+export default App;
